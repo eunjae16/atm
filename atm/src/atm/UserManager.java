d12c38292b61f4ab275b1adb8d8b93dfeeac3842
EZEN <EZEN@DESKTOP-DTU8P03>
2023-03-14 16:18:54 +0900

    create project atm

--- atm/src/atm/UserManager.java
+++ atm/src/atm/UserManager.java
@@ -1,5 +1,36 @@
 package atm;
 
-public class UserManager {
+import java.util.ArrayList;
 
+public class UserManager {
+	private static ArrayList<User> list = new ArrayList<>();
+	// User에 대한
+	
+	// Create
+	public void addUser(User user) {
+		this.list.add(user);
+	}
+	// Read
+	public User getUser(int index) {
+		User user = this.list.get(index);
+		
+		//사본 제공
+		User requestObject = new User();
+		return requestObject;
+	}
+	
+	public User getUserById(String id) {
+		int index = -1; // ?
+		return getUser(index);
+	}
+	
+	// Update
+	public void setUser(int index, User user) {
+		this.list.set(index, user);
+	}
+	
+	// Delete
+	public void deleteUserById(int index) {
+		this.list.remove(index);
+	}
 }
