7bc4b6cdd769e55d2e6641942bbcb204fe11b0fb
EZEN <EZEN@DESKTOP-DTU8P03>
2023-03-14 18:20:47 +0900

    save project

--- atm/src/atm/User.java
+++ atm/src/atm/User.java
@@ -1,5 +1,40 @@
 package atm;
 
-public class User {
+import java.util.ArrayList;
 
+public class User {
+	private String id;
+	private String password;
+	private String name;
+	private ArrayList<Account> accs;
+	
+	public User(String id, String password, String name){
+		this.id = id;
+		this.password = password;
+		this.name = name;
+	}
+	public User() {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public ArrayList<Account> getAccs() {
+		return this.accs;
+	}
+	public void setAccs(ArrayList<Account> accs) {
+		this.accs = accs;
+	}
 }
